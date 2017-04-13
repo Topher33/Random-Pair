71d666e6994579344727bb00f169381a40c32e74
Matt Warren <Matthew_Warren@homedepot.com>
2017-04-13 15:34:41 -0400

    WIP need to test integration to SLACK

--- integrations.py
+++ integrations.py
@@ -2,6 +2,12 @@ import requests
 import json
 
 class Slack(pairs_list):
-    j_data = (pairs_list)
+
     slack_url = '<some slack hook url>'
+    j_template = 'Pair for this week is #{teammate_1} and #{teammate_2}'
+
+    def send_message(pairs_list)
+    teammate_1 = pairs_list[0]
+    teammate_2 = pairs_list[1]
+    j_data = j_template
     r = requests.post(slack_url, data = j_data)
